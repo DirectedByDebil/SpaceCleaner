e6093838aefaa8ebcdd36babcd62425f075021b9
vermenaard7 <vermenaard7@mail.ru>
2025-02-18 20:33:47 +0300

    Add connection between UI and game state in PlayerHealthView

--- Assets/ProjectRoot/Scripts/Combat/PlayerHealthView.cs
+++ Assets/ProjectRoot/Scripts/Combat/PlayerHealthView.cs
@@ -1,21 +1,40 @@
-﻿using Core;
+﻿using Characters;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Combat
 {
     public class PlayerHealthView : MonoBehaviour, IHealthView
     {
+        public UIDocument uiDocument;
+
+        private Player _player;
+        private VisualElement _hpBar;
+        private float _hpBarWidthCoef = 0;
+
+        private Label _hpBarNumberLabel;
+
+        void OnEnable()
+        {
+            _player = GetComponent<Player>();
+            //Находим кнопку таким запросом, в параметр передаем имя кнопки
+            _hpBar = uiDocument.rootVisualElement.Q<VisualElement>("hpBar");
+            // Register a callback for the GeometryChangedEvent
+            _hpBar.RegisterCallback<GeometryChangedEvent>(OnGeometryChanged);
+
+            _hpBarNumberLabel = uiDocument.rootVisualElement.Q<Label>("hpText");
+            _hpBarNumberLabel.text = _player.HealthStats.MaxHp.ToString();
+        }
 
         public void OnHealthChanged(int health)
         {
-
-            GUIOutput.AddOutput("HP", health);
+            _hpBarNumberLabel.text = health.ToString();
+            _hpBar.style.width = new StyleLength(health * _hpBarWidthCoef);
         }
 
 
         public void OnHealthEnded()
         {
-
             gameObject.SetActive(false);
         }
 
@@ -23,6 +42,20 @@ namespace Combat
         public void OnShieldEnded()
         {
 
+        }
+
+        // Callback for the GeometryChangedEvent
+        public void OnGeometryChanged(GeometryChangedEvent evt)
+        {
+            if (_hpBarWidthCoef > 0)
+            {
+                return;
+            }
+            // Get the VisualElement that triggered the event
+            VisualElement element = evt.target as VisualElement;
+
+            // Get the resolved style of the VisualElement
+            _hpBarWidthCoef = element.resolvedStyle.width / _player.HealthStats.MaxHp;
         }
     }
 }
