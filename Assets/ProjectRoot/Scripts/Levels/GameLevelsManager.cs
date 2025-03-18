c0e9d06f77c5749c6a716a1032b667d1c374e34e
Big Boss Arsenii <chromov-arsenij@mail.ru>
2025-03-18 21:59:00 +0300

    fix(project): fixed version troubles

--- Assets/ProjectRoot/Scripts/Levels/GameLevelsManager.cs
+++ Assets/ProjectRoot/Scripts/Levels/GameLevelsManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 using System.Collections.Generic;
 
 namespace Levels
@@ -10,16 +11,29 @@ namespace Levels
         [SerializeField, TextArea(1, 2), Space]
         private List<string> _levels = new();
 
+        [SerializeField, Space]
+        private TextMeshProUGUI _textHandler;
+
 
         private void Awake()
         {
-            
+            /*
+                foreach(string level in _levels)
+                {
+
+                    _textHandler.text += level + "\n";
+                }
+            */
+
+            _levels = new List<string>()
+            {
+
+                "MainMenu",
+                "CleaningLevel",
+                "CleaningArena"
+            };
+
             GameLevels.SetLevels(_levels);
-        }
-
-
-        private void Start()
-        {
 
             SceneManager.LoadSceneAsync(_levels[0]);
         }
